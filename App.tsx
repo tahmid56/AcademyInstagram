fbab92545ab828bbfa5b4805715b88cf54bc85f3
Tahmid Akter <tahmidakter56@gmail.com>
2023-02-17 18:06:55 +0600

    init project & install vector icons

---
+++ App.tsx
@@ -0,0 +1,18 @@
+
+import { Text, View } from "react-native";
+import colors from "./src/theme/colors";
+import fonts from "./src/theme/fonts";
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const App = () =>{
+    return (
+        <View style={{flex:1, alignItems: "center",justifyContent: "center"}}>
+            <Text style={{color: colors.primary, fontSize: fonts.size.xxl}}>
+                Hello world            
+                <AntDesign name="stepforward" size={25}/>
+            </Text>
+        </View>
+    );
+};
+
+export default App;
---
+++ src/theme/fonts.ts
@@ -0,0 +1,17 @@
+const size = {
+    xs: 10,
+    sm:12,
+    default: 14,
+    md: 16,
+    lg: 20,
+    xl: 24,
+    xxl: 30,
+}
+const weight = {
+    fill: "900",
+    semi: "600",
+    bold: 'bold',
+    normal: "normal",
+    this: "400"
+}
+export default {size, weight};
